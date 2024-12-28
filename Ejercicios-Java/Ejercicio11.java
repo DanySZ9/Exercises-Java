236f2402a25469b8687f25e20637cd2731d4f478
DanySZ9 <dany.martinez.since09@gmail.com>
2024-12-28 17:37:22 -0600

    Agregue comentarios al codigo

--- Ejercicios-Java/Ejercicio11.java
+++ Ejercicios-Java/Ejercicio11.java
@@ -8,9 +8,13 @@ public class Ejercicio11 {
         int sueldoMaximo = 0;
         boolean primerSueldo = false;
 
+        /*
+         * Converciona el comando de JOptionPane de String a Int para que el valor que se introduzca sea de valor numerico
+         * para la variable de las cantidades de sueldos que se desean ingresar
+         */
         cantidadSueldos=Integer.parseInt(JOptionPane.showInputDialog("¿Cuantos sueldos desea ingresar?"));
         for(int i =1; i<=cantidadSueldos; i++) {
-            sueldo=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el sueldo No. " + i));
+            sueldo=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el sueldo No. " + i));   // Converciona de String a Int para la variable de sueldo
             if(primerSueldo==false) {
                 sueldoMaximo = sueldo;
                 primerSueldo =true;
@@ -19,6 +23,6 @@ public class Ejercicio11 {
                 sueldoMaximo = sueldo;
             }
         }
-        JOptionPane.showMessageDialog(null, "El sueldo mayor es: " + sueldoMaximo);
+        JOptionPane.showMessageDialog(null, "El sueldo mayor es: " + sueldoMaximo);   // Se muestra un mensaje donde se muestra el sueldo mayor de todos los ingresados
     }   
 }
