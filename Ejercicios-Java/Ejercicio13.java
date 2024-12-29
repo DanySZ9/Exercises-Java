e0a6934361ea7ae07cbdc731b7333c41ca017b88
DanySZ9 <dany.martinez.since09@gmail.com>
2024-12-28 22:15:32 -0600

    Ejercicio finalizado

---
+++ Ejercicios-Java/Ejercicio13.java
@@ -0,0 +1,21 @@
+// Guardar 5 numeros en un arreglo y mostrarlos en orden inversa
+import java.util.Scanner;
+public class Ejercicio13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¿Cuantos numeros desea ingresar?");
+        int cantidad = scanner.nextInt();
+        System.out.println("Ingrese los numeros: ");
+        int numeros[] = new int[cantidad];
+
+        for(int i=0; i<cantidad; i++) {
+            numeros[i] = scanner.nextInt();
+        }
+        
+        System.out.println("Números invertidos:");
+        for (int i = cantidad - 1; i >= 0; i--) {
+            System.out.println(numeros[i]);
+        }
+        scanner.close();
+    }
+}
