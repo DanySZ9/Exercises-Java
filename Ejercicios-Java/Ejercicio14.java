a4c8c849ace06443129fca6d562a8fe134e75184
DanySZ9 <dany.martinez.since09@gmail.com>
2024-12-28 23:08:55 -0600

    Ejercicio pendiente.

---
+++ Ejercicios-Java/Ejercicio14.java
@@ -0,0 +1,18 @@
+/*
+ * Guardar 10 numeros en un arreglo y al final mostrarlos en pantalla en el siguiente orden
+ * El primero
+ * El ultimo
+ * El segundo
+ * El penultimo
+ * El tercero
+ * etc...
+ * En esta version se realizara primero haciendo uso de la terminal para observar los resultados
+ */
+import java.util.Scanner;
+public class Ejercicio14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
