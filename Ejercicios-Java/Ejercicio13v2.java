2bb5fc6c792b2e7455cd4b29cb0e871b54d0d060
DanySZ9 <dany.martinez.since09@gmail.com>
2024-12-28 22:54:30 -0600

    En esta version se implementa el uso de las ventanas emergentes para ingresar los numeros y observarlos de manera inversa

---
+++ Ejercicios-Java/Ejercicio13v2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class Ejercicio13v2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cantidad=Integer.parseInt(JOptionPane.showInputDialog("¿Cuantos numeros desea ingresar?"));
+        StringBuilder instruccion = new StringBuilder("Ingrese los numeros: ");
+
+        int numeros[] = new int[cantidad];
+
+        for(int i=0; i<cantidad; i++) {
+            numeros[i]=Integer.parseInt(JOptionPane.showInputDialog(instruccion));
+        }
+        StringBuilder mensaje = new StringBuilder("Numeros invertidos:\n");
+        for (int i = cantidad - 1; i >= 0; i--) {
+            mensaje.append("- ").append(numeros[i]).append("\n");
+        }
+        JOptionPane.showMessageDialog(null, mensaje);
+        scanner.close();        
+    }
+}
