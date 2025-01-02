72312ddf2a5a5f141737caa31286a599963bd357
DanySZ9 <dany.martinez.since09@gmail.com>
2025-01-02 10:22:12 -0600

    Comprender mejor el ejercicio o realizarlo nuevamente y tambien agregar comentarios

--- Ejercicios-Java/ArregloCrecienteOrdenados.java
+++ Ejercicios-Java/ArregloCrecienteOrdenados.java
@@ -10,8 +10,35 @@
  * Sin alterar el orden de la tabla
  */
 
+ import java.util.Scanner;
 public class ArregloCrecienteOrdenados {
     public static void main(String[]args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int arr[] = new int[5];
+
+        System.out.println("Introduce los 5 numeros (ordenados de forma creciente)");
+        for (int i=0; i<5; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Ingrese un numero entero: " );
+        int num = scanner.nextInt();
+        int pos_num = 0;
+        int k = 0;
+
+        while (arr[k]<num && k<=4) {
+            pos_num++;
+            k++;
+        }
+        for(int i=4;i<pos_num; i--) {
+            arr[i+1] = arr[i];
+        }
+        arr[pos_num] = num;
+        System.out.println("Nuevo arreglo ordenado: ");
+        for(int i=0; i<5; i++) {
+            System.out.println(arr[i]);
+        }
+
+        scanner.close();
     }
 }
